04b45375c9707248423146bda2bd61a04525734a
nkarl <21993921+nkarl@users.noreply.github.com>
2023-05-28 16:35:59 -0700

    Implemented @RestController for a simple Response containing list of courses.

--- src/main/java/com/example/demo/LearnSpringBootApp.java
+++ src/main/java/com/example/demo/LearnSpringBootApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LearnSpringBootApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootApp.class, args);
+    }
 
 }
